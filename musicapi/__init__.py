7a8e59c13300b98fa8f188d367786b17e4bbaf3a
luren <3308862290@qq.com>
2023-05-25 17:28:47 +0800

    init: 初始化仓库

---
+++ musicapi/__init__.py
@@ -0,0 +1,14 @@
+"""
+MusicApi Library
+~~~~~~~~~~~~~~~~~~~~~
+Api for QQMusic
+
+:license: MIT, see LICENSE for more details.
+"""
+from .__version__ import (
+    __title__,
+    __description__,
+    __author__,
+    __author_email__,
+    __license__,
+)
