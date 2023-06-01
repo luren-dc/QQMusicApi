6d6cb4cee8f96aff6d66696c3c0fa23ea5a0c2bd
Luren <3308862290@qq.com>
2023-06-01 13:59:25 +0800

    feat: api自定义搜索

--- qqmusicapi/app.py
+++ qqmusicapi/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from qqmusicapi.api.search import Search
+from flask import request
 
 app = Flask(__name__)
 
@@ -10,9 +11,18 @@ def index():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/search/<search_type>")
-def search(search_type: str = "song"):
-    return Search.search("k", search_type=search_type, page=1)
+@app.route("/search/<search_type>", methods=["GET"])
+def search(search_type: str):
+    query = request.args.get("query", "")
+    try:
+        num = int(request.args.get("num", 1))
+    except ValueError:
+        num = 10
+    try:
+        page = int(request.args.get("page", 1))
+    except ValueError:
+        page = 1
+    return Search.search(query, search_type=search_type, page=page, num=num)
 
 
 def main():
