008f19c4fb4985c03edc55cfba93849f72ab82f5
luren-dc <3308862290@qq.com>
2023-12-02 21:31:35 +0800

    feat: aiohttp.seesion复用

--- pyqqmusicapi/qqmusic.py
+++ pyqqmusicapi/qqmusic.py
@@ -1,3 +1,5 @@
+import asyncio
+import atexit
 import json
 import threading
 from typing import Dict
@@ -9,12 +11,31 @@ from .exceptions import ApiRequestException, MusicTokenException
 from .qimei import Qimei
 from .utils import random_string
 
+_aiohttp_session_pool: Dict[asyncio.AbstractEventLoop, aiohttp.ClientSession] = {}
 _thread_lock = threading.Lock()
+
+
+async def get_aiohttp_session() -> aiohttp.ClientSession:
+    """
+    获取 aiohttp.ClientSession
+
+    Returns:
+        aiohttp.ClientSession
+    """
+    loop = asyncio.get_event_loop()
+    session = _aiohttp_session_pool.get(loop, None)
+    if session is None:
+        session = aiohttp.ClientSession(
+            loop=loop, connector=aiohttp.TCPConnector(ssl=False)
+        )
+        _aiohttp_session_pool[loop] = session
+
+    return session
 
 
 class QQMusic:
     """
-    请求QQ音乐的核心接口，仅创建一次
+    请求QQ音乐的核心模块，仅创建一次
     """
 
     _instance = None
@@ -30,7 +51,7 @@ class QQMusic:
         musickey: str = "",
     ):
         """
-        初始化QQ音乐核心接口
+        初始化QQ音乐核心模块
 
         Args:
             musicid: QQ音乐ID
@@ -48,8 +69,6 @@ class QQMusic:
             self._qimei36 = "cc8d07a748d4be0a8b91eacd100014a1730e"
         else:
             self._qimei36 = qimei.q36
-
-        self._client = aiohttp.ClientSession()
 
         self._uid = random_string(10, "0123456789")
 
@@ -71,12 +90,12 @@ class QQMusic:
             self.musickey = musickey
 
     async def get(self, *args, **kwargs) -> aiohttp.ClientResponse:
-        async with aiohttp.ClientSession() as session:
-            return await session.get(*args, **kwargs)
+        session = await get_aiohttp_session()
+        return await session.get(*args, **kwargs)
 
     async def post(self, *args, **kwargs) -> aiohttp.ClientResponse:
-        async with aiohttp.ClientSession() as session:
-            return await session.post(*args, **kwargs)
+        session = await get_aiohttp_session()
+        return await session.post(*args, **kwargs)
 
     async def get_data(self, module: str, method: str, param: Dict, **kwargs) -> Dict:
         # 构造公用参数
@@ -125,12 +144,12 @@ class QQMusic:
         formated_data = json.dumps(data, separators=(",", ":"), ensure_ascii=False)
 
         # 请求API
-        async with aiohttp.ClientSession() as session:
-            async with session.post(
-                "https://u.y.qq.com/cgi-bin/musicu.fcg",
-                data=formated_data.encode("utf-8"),
-            ) as response:
-                res = json.loads(await response.text(kwargs.get("charset", "utf-8")))
+        session = await get_aiohttp_session()
+        async with session.post(
+            "https://u.y.qq.com/cgi-bin/musicu.fcg",
+            data=formated_data.encode("utf-8"),
+        ) as response:
+            res = json.loads(await response.text(kwargs.get("charset", "utf-8")))
 
         # 返回请求数据
         code = res["request"].get("code", 0)
@@ -140,3 +159,24 @@ class QQMusic:
         if not res_data:
             raise MusicTokenException("获取接口数据失败，请检查提交的数据")
         return res_data
+
+
+@atexit.register
+def __clean() -> None:
+    """
+    程序退出清理操作。
+    """
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        return
+
+    async def __clean_task():
+        s0 = _aiohttp_session_pool.get(loop, None)
+        if s0 is not None:
+            await s0.close()
+
+    if loop.is_closed():
+        loop.run_until_complete(__clean_task())
+    else:
+        loop.create_task(__clean_task())
