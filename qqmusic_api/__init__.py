2d98e6ee0978ab1906d6f03dcdedebd6510d1737
luren-dc <dluren.c@gmail.com>
2024-06-02 15:46:36 +0800

    doc: 更新 READEM

--- qqmusic_api/__init__.py
+++ qqmusic_api/__init__.py
@@ -1,4 +1,4 @@
-from .api import album, mv, login, search, song, songlist
+from .api import album, mv, login, search, song, songlist, top
 from .utils.credential import Credential
 
-__all__ = ["album", "Credential", "login", "mv", "search", "song", "songlist"]
+__all__ = ["album", "Credential", "login", "mv", "search", "song", "songlist", "top"]
