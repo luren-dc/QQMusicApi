dc9cbcf1fcffe58421e9a9bc1fd2cd8d54c9775b
Luren <68656403+luren-dc@users.noreply.github.com>
2024-06-07 01:57:55 +0800

    Revert "合并 dev 分支"

--- qqmusic_api/__init__.py
+++
@@ -1,4 +0,0 @@
-from .api import album, mv, login, search, song, songlist, top
-from .utils.credential import Credential
-
-__all__ = ["album", "Credential", "login", "mv", "search", "song", "songlist", "top"]
