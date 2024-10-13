40a4cf97e0d2d1ad424fe8cc2ea607fbc43acaca
Luren <68656403+luren-dc@users.noreply.github.com>
2024-10-13 11:06:30 +0800

    feat: 支持获取专辑封面链接 (#70)

--- qqmusic_api/album.py
+++ qqmusic_api/album.py
@@ -1,11 +1,26 @@
 """专辑相关 API"""
 
-from typing import Optional
+from typing import Literal, Optional
 
 from .utils.network import Api
 from .utils.utils import get_api
 
 API = get_api("album")
+
+
+def get_album_cover(mid: str, size: Literal[150, 300, 500, 800] = 300) -> str:
+    """获取专辑封面链接
+
+    Args:
+        mid: 专辑 mid
+        size: 封面大小
+
+    Returns:
+        封面链接
+    """
+    if size not in [150, 300, 500, 800]:
+        raise ValueError("not supported size")
+    return f"https://y.gtimg.cn/music/photo_new/T002R{size}x{size}M000{mid}.jpg"
 
 
 class Album:
