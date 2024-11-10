c9d7179a2b50784fd9aeb1dc13f756cd1c67acb5
Luren <68656403+luren-dc@users.noreply.github.com>
2024-11-10 13:42:16 +0800

    chore(release): release v0.1.11

--- qqmusic_api/__init__.py
+++ qqmusic_api/__init__.py
@@ -5,7 +5,7 @@ from .utils.credential import Credential
 from .utils.network import get_session, set_session
 from .utils.sync import sync
 
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 
 logger = logging.getLogger("qqmusicapi")
 
