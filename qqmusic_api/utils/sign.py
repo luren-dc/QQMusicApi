50467b644555b99c5b38b8237fcc75bbf5ae4848
Luren <68656403+luren-dc@users.noreply.github.com>
2024-12-22 04:54:25 +0000

    🐛 fix: 数据不匹配导致 sign 错误

--- qqmusic_api/utils/sign.py
+++ qqmusic_api/utils/sign.py
@@ -1,67 +1,22 @@
 """QQ音乐 sign"""
 
+import base64
 import json
-import re
 
 from .common import calc_md5
 
 
-def _v(b):
-    res = []
+def _head(b: bytes) -> list:
     p = [21, 4, 9, 26, 16, 20, 27, 30]
-    for x in p:
-        res.append(b[x])
-    return "".join(res)
+    return [b[x] for x in p]
 
 
-def _c(b):
-    res = []
+def _tail(b: bytes) -> list:
     p = [18, 11, 3, 2, 1, 7, 6, 25]
-    for x in p:
-        res.append(b[x])
-    return "".join(res)
+    return [b[x] for x in p]
 
 
-def _y(a, b, c):
-    e = []
-    r25 = a >> 2
-    if b is not None and c is not None:
-        r26 = a & 3
-        r26_2 = r26 << 4
-        r26_3 = b >> 4
-        r26_4 = r26_2 | r26_3
-        r27 = b & 15
-        r27_2 = r27 << 2
-        r27_3 = r27_2 | (c >> 6)
-        r28 = c & 63
-        e.append(r25)
-        e.append(r26_4)
-        e.append(r27_3)
-        e.append(r28)
-    else:
-        r10 = a >> 2
-        r11 = a & 3
-        r11_2 = r11 << 4
-        e.append(r10)
-        e.append(r11_2)
-    return e
-
-
-def _n(ls):
-    e = []
-    for i in range(0, len(ls), 3):
-        if i < len(ls) - 2:
-            e += _y(ls[i], ls[i + 1], ls[i + 2])
-        else:
-            e += _y(ls[i], None, None)
-    res = []
-    b64all = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
-    for i in e:
-        res.append(b64all[i])
-    return "".join(res)
-
-
-def _t(b):
+def _middle(b: bytes) -> list:
     zd = {
         "0": 0,
         "1": 1,
@@ -84,8 +39,8 @@ def _t(b):
     res = []
     j = 0
     for i in range(0, len(b), 2):
-        one = zd[b[i]]
-        two = zd[b[i + 1]]
+        one = zd[chr(b[i])]
+        two = zd[chr(b[i + 1])]
         r = one * 16 ^ two
         res.append(r ^ ol[j])
         j += 1
@@ -101,12 +56,12 @@ def sign(request: dict) -> str:
     Returns:
         签名结果
     """
-    md5_str = calc_md5(json.dumps(request)).upper()
-    h = _v(md5_str)
-    e = _c(md5_str)
-    ls = _t(md5_str)
-    m = _n(ls)
-    res = "zzb" + h + m + e
-    res = res.lower()
-    r = re.compile(r"[\\/+]")
-    return re.sub(r, "", res)
+    md5_str = calc_md5(json.dumps(request, ensure_ascii=False, separators=(",", ":"))).upper().encode("utf-8")
+
+    h = _head(md5_str)
+    e = _tail(md5_str)
+    ls = _middle(md5_str)
+    m = base64.b64encode(bytes(ls)).decode("utf-8")
+
+    res = "zzb" + "".join(map(chr, h)) + m + "".join(map(chr, e))
+    return res.lower().replace("/", "").replace("+", "").replace("=", "")
