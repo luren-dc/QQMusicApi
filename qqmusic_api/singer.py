42e8b9bffb1c73666256c9aaf82b92cd9d4cb8c4
Luren <68656403+luren-dc@users.noreply.github.com>
2025-02-02 14:35:36 +0000

    ♻️ refactor: 重构 Singer API

--- qqmusic_api/singer.py
+++ qqmusic_api/singer.py
@@ -1,25 +1,13 @@
 """歌手相关 API"""
 
 from enum import Enum
-from typing import Literal
+from typing import Any, Literal, cast
 
-from .utils.common import get_api
-from .utils.network import Api
-
-API = get_api("singer")
+from .utils.network import api_request
 
 
 class AreaType(Enum):
-    """地区
-
-    + ALL:     全部
-    + CHINA:   内地
-    + TAIWAN:  台湾
-    + AMERICA: 美国
-    + EUROPE:  欧美
-    + JAPAN:   日本
-    + KOREA:   韩国
-    """
+    """地区类型枚举"""
 
     ALL = -100
     CHINA = 200
@@ -31,23 +19,7 @@ class AreaType(Enum):
 
 
 class GenreType(Enum):
-    """风格
-
-    + ALL:           全部
-    + POP:           流行
-    + RAP:           说唱
-    + CHINESE_STYLE: 国风
-    + ROCK:          摇滚
-    + ELECTRONIC:    电子
-    + FOLK:          民谣
-    + R_AND_B:       R&B
-    + ETHNIC:        民族乐
-    + LIGHT_MUSIC:   轻音乐
-    + JAZZ:          爵士
-    + CLASSICAL:     古典
-    + COUNTRY:       乡村
-    + BLUES:         蓝调
-    """
+    """风格类型枚举"""
 
     ALL = -100
     POP = 7
@@ -66,13 +38,7 @@ class GenreType(Enum):
 
 
 class SexType(Enum):
-    """性别
-
-    + ALL:    全部
-    + MALE:   男
-    + FEMALE: 女
-    + GROUP:  组合
-    """
+    """性别类型枚举"""
 
     ALL = -100
     MALE = 0
@@ -81,18 +47,7 @@ class SexType(Enum):
 
 
 class TabType(Enum):
-    """Tab 类型
-
-    + WIKI:     wiki
-    + ALBUM:    专辑
-    + COMPOSER: 作曲
-    + LYRICIST: 作词
-    + PRODUCER: 制作人
-    + ARRANGER: 编曲
-    + MUSICIAN: 乐手
-    + SONG:     歌曲
-    + VIDEO:    视频
-    """
+    """Tab 类型枚举"""
 
     WIKI = ("wiki", "IntroductionTab")
     ALBUM = ("album", "AlbumTab")
@@ -104,120 +59,101 @@ class TabType(Enum):
     SONG = ("song_sing", "SongTab")
     VIDEO = ("video", "VideoTab")
 
-    def __init__(self, tab_id: str, tab_name: str) -> None:
+    def __init__(self, tab_id: str, tab_name: str):
         self.tab_id = tab_id
         self.tab_name = tab_name
 
 
+@api_request("music.musichallSinger.SingerList", "GetSingerList")
 async def get_singer_list(
     area: AreaType = AreaType.ALL,
     sex: SexType = SexType.ALL,
     genre: GenreType = GenreType.ALL,
-) -> list:
+):
     """获取歌手列表
 
     Args:
-        area:  地区.Defaluts to AreaType.ALL
-        sex:   性别.Defaluts to SexType.ALL
-        genre: 风格.Defaluts to GenreType.ALL
-
-    Returns:
-        歌手列表
+        area: 地区
+        sex: 性别
+        genre: 风格
     """
-    result = (
-        await Api(**API["singer_list"])
-        .update_params(
-            hastag=0,
-            area=area.value,
-            sex=sex.value,
-            genre=genre.value,
-        )
-        .result
+    return {
+        "hastag": 0,
+        "area": area.value,
+        "sex": sex.value,
+        "genre": genre.value,
+    }, lambda data: cast(
+        list[dict[str, Any]],
+        data["hotlist"],
     )
-    return result["hotlist"]
 
 
-class Singer:
-    """歌手类
+@api_request("music.UnifiedHomepage.UnifiedHomepageSrv", "GetHomepageHeader")
+async def get_info(mid: str):
+    """获取歌手基本信息
 
     Args:
         mid: 歌手 mid
     """
+    return {"SingerMid": mid}, lambda data: data
 
-    def __init__(self, mid: str) -> None:
-        """初始化歌手类
 
-        Args:
-            mid: 歌手 mid
-        """
-        self.mid = mid
-        self._info: dict | None = None
+@api_request("music.UnifiedHomepage.UnifiedHomepageSrv", "GetHomepageTabDetail")
+async def get_tab_detail(mid: str, tab_type: TabType, page: int = 1, num: int = 10):
+    """获取歌手 Tab 详细信息
 
-    async def get_info(self) -> dict:
-        """获取歌手信息
+    Args:
+        mid: 歌手 mid
+        tab_type: Tab 类型
+        page: 页码
+        num: 返回数量
+    """
+    params = {
+        "SingerMid": mid,
+        "IsQueryTabDetail": 1,
+        "TabID": tab_type.tab_id,
+        "PageNum": page - 1,
+        "PageSize": num,
+        "Order": 0,
+    }
 
-        Returns:
-            歌手信息
-        """
-        if not self._info:
-            self._info = await Api(**API["homepage"]).update_params(SingerMid=self.mid).result
-        return self._info
+    def _processor(data: dict[str, Any]) -> list[dict[str, Any]]:
+        data = data[tab_type.tab_name]
+        return data.get("List", data.get("VideoList", data.get("AlbumList", data)))
 
-    async def get_tab_detail(self, tab_type: TabType, page: int = 1, num: int = 100) -> dict:
-        """获取歌手主页 Tab 详细信息
+    return params, _processor
 
-        Args:
-            tab_type: Tab 类型
-            page:     页码
-            num:      返回数量
 
-        Returns:
-            Tab 详细信息
-        """
-        return (
-            await Api(**API["homepage_tab_detail"])
-            .update_params(
-                SingerMid=self.mid,
-                IsQueryTabDetail=1,
-                TabID=tab_type.tab_id,
-                PageNum=page - 1,
-                PageSize=num,
-                Order=0,
-            )
-            .result
-        )[tab_type.tab_name]
+@api_request("music.musichallSinger.SingerInfoInter", "GetSingerDetail")
+async def get_desc(mids: list[str]):
+    """获取歌手简介
 
-    async def get_wiki(self) -> dict:
-        """获取歌手WiKi
+    Args:
+        mids: 歌手 mid 列表
+    """
+    return {"singer_mids": mids, "groups": 1, "wikis": 1}, lambda data: cast(list[dict[str, Any]], data["singer_list"])
 
-        Returns:
-            歌手WiKi
-        """
-        return await self.get_tab_detail(TabType.WIKI)
 
-    async def get_song(
-        self,
-        type: Literal[
-            TabType.SONG,
-            TabType.ALBUM,
-            TabType.COMPOSER,
-            TabType.LYRICIST,
-            TabType.PRODUCER,
-            TabType.ARRANGER,
-            TabType.MUSICIAN,
-        ] = TabType.SONG,
-        page: int = 1,
-        num: int = 100,
-    ) -> list[dict]:
-        """获取歌手歌曲
+async def get_songs(
+    mid: str,
+    tab_type: Literal[
+        TabType.SONG,
+        TabType.ALBUM,
+        TabType.COMPOSER,
+        TabType.LYRICIST,
+        TabType.PRODUCER,
+        TabType.ARRANGER,
+        TabType.MUSICIAN,
+    ] = TabType.SONG,
+    page: int = 1,
+    num: int = 10,
+) -> list[dict[str, Any]]:
+    """获取歌手歌曲
 
-        Args:
-            type: Tab 类型
-            page: 页码
-            num:  返回数量
-
-        Returns:
-            歌曲信息列表
-        """
-        data = await self.get_tab_detail(type, page, num)
-        return data["List"]
+    Args:
+        mid: 歌手 mid
+        tab_type: Tab 类型
+        page: 页码
+        num:  返回数量
+    """
+    return await get_tab_detail(mid, tab_type, page, num)
