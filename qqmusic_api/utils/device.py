d33a1e5e8e23c396180a6bb97033eb4af76770a5
Luren <68656403+luren-dc@users.noreply.github.com>
2025-02-02 05:19:38 +0000

    ✨ feat!: 修改请求逻辑实现方式,实现`RequestGroup`

--- qqmusic_api/utils/device.py
+++ qqmusic_api/utils/device.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from typing import ClassVar
 from uuid import uuid4
 
-device_path = Path(__file__).parent.parent / "data" / "device.json"
+device_path = Path(__file__).parent.parent / ".cache" / "device.json"
 
 
 def random_imei() -> str:
@@ -96,4 +96,5 @@ def get_cached_device() -> Device:
 
 def save_device(device: Device):
     """缓存 Device"""
+    device_path.parent.mkdir(parents=True, exist_ok=True)
     device_path.write_text(json.dumps(asdict(device)))
