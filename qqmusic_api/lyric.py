098acac76be9ccd58ac1274d42d43142e0b79152
Luren <68656403+luren-dc@users.noreply.github.com>
2025-02-02 08:27:59 +0000

    ♻️ refactor: 重构 Lyric API

--- qqmusic_api/lyric.py
+++ qqmusic_api/lyric.py
@@ -1,41 +1,33 @@
 """歌词 API"""
 
 import re
+from typing import Any
 
-from .utils.common import get_api, qrc_decrypt
-from .utils.network import Api
+from qqmusic_api.utils.network import api_request
 
-API = get_api("lyric")
+from .utils.common import qrc_decrypt
 
 QRC_PATTERN = re.compile(r'<Lyric_.* LyricType=".*" LyricContent="(?P<content>.*?)"/>', re.DOTALL)
 
 
+@api_request("music.musichallSong.PlayLyricInfo", "GetPlayLyricInfo")
 async def get_lyric(
-    *,
-    mid: str | None = None,
-    id: int | None = None,
+    value: str | int,
     qrc: bool = False,
     trans: bool = False,
     roma: bool = False,
-) -> dict[str, str]:
+):
     """获取歌词
 
-    Note:
-        歌曲 mid 和 id,两者至少提供一个
-
     Args:
-        mid:   歌曲 mid
-        id:    歌曲 id
-        qrc:   是否返回逐字歌词
+        value: 歌曲 ID 或 MID
+        qrc: 是否返回逐字歌词
         trans: 是否返回翻译歌词
-        roma:  是否返回罗马歌词
+        roma: 是否返回罗马歌词
 
     Returns:
         {"lyric": 歌词或逐字歌词, "trans": 翻译歌词, "roma": 罗马歌词}
     """
-    if mid is None and id is None:
-        raise ValueError("mid or id must be provided")
-
     params = {
         "crypt": 1,
         "ct": 11,
@@ -45,24 +37,35 @@ async def get_lyric(
         "qrc_t": 0,
         "roma": roma,
         "roma_t": 0,
-        "songId": id,
-        "songMid": mid,
         "trans": trans,
         "trans_t": 0,
         "type": 1,
     }
-    res = await Api(**API["info"]).update_params(**params).result
 
-    lyric = qrc_decrypt(res["lyric"])
-    
+    if isinstance(value, int):
+        params["songId"] = value
+    else:
+        params["songMid"] = value
 
-    if lyric and qrc:
-        m_qrc = QRC_PATTERN.search(lyric)
-        if  m_qrc and m_qrc.group("content"):
-            lyric = m_qrc.group("content")
+    def _processor(data: dict[str, Any]):
+        lyric = qrc_decrypt(data["lyric"])
+        trans = qrc_decrypt(data["trans"])
+        roma = qrc_decrypt(data["roma"])
 
-    return {
-        "lyric": lyric,
-        "trans": qrc_decrypt(res["trans"]),
-        "roma": qrc_decrypt(res["roma"]),
-    }
+        if lyric and qrc:
+            m_qrc = QRC_PATTERN.search(lyric)
+            if m_qrc and m_qrc.group("content"):
+                lyric = m_qrc.group("content")
+
+        if roma:
+            m_roma = QRC_PATTERN.search(roma)
+            if m_roma and m_roma.group("content"):
+                roma = m_roma.group("content")
+
+        return {
+            "lyric": lyric,
+            "trans": trans,
+            "roma": roma,
+        }
+
+    return params, _processor
