e50698476acfd7f854f48a6e5b0684625a407ae8
Luren <68656403+luren-dc@users.noreply.github.com>
2025-02-02 07:06:14 +0000

    ♻️ refactor: 重构Search API

--- qqmusic_api/search.py
+++ qqmusic_api/search.py
@@ -1,25 +1,25 @@
 """搜索相关 API"""
 
 from enum import Enum
+from typing import Any
 
-from .utils.common import get_api, get_searchID
-from .utils.network import Api
-
-API = get_api("search")
+from .utils.common import get_searchID
+from .utils.network import NO_PROCESSOR, api_request
+from .utils.session import get_session
 
 
 class SearchType(Enum):
     """搜索类型
 
-    + SONG:       歌曲
-    + SINGER:     歌手
-    + ALBUM:      专辑
-    + SONGLIST:   歌单
-    + MV:         MV
-    + LYRIC:      歌词
-    + USER:       用户
+    + SONG: 歌曲
+    + SINGER: 歌手
+    + ALBUM: 专辑
+    + SONGLIST: 歌单
+    + MV: MV
+    + LYRIC: 歌词
+    + USER: 用户
     + AUDIO_SONG: 节目专辑
-    + AUDIO:      节目
+    + AUDIO: 节目
     """
 
     SONG = 0
@@ -33,18 +33,18 @@ class SearchType(Enum):
     AUDIO = 18
 
 
-async def hotkey() -> list[dict]:
+@api_request("music.musicsearch.HotkeyService", "GetHotkeyForQQMusicMobile")
+async def hotkey():
     """获取热搜词
 
     Returns:
         热搜词列表
     """
-    params = {"search_id": get_searchID()}
-    res = await Api(**API["hotkey"]).update_params(**params).result
-    return res["vec_hotkey"]
+    return {"search_id": get_searchID()}, NO_PROCESSOR
 
 
-async def complete(keyword: str) -> list[str]:
+@api_request("music.smartboxCgi.SmartBoxCgi", "GetSmartBoxResult")
+async def complete(keyword: str):
     """搜索词补全
 
     Args:
@@ -53,17 +53,15 @@ async def complete(keyword: str) -> list[str]:
     Returns:
         补全结果
     """
-    params = {
+    return {
         "search_id": get_searchID(),
         "query": keyword,
         "num_per_page": 0,
         "page_idx": 0,
-    }
-    res = await Api(**API["complete"]).update_params(**params).result
-    return res["items"]
+    }, NO_PROCESSOR
 
 
-async def quick_search(keyword: str) -> dict:
+async def quick_search(keyword: str) -> dict[str, Any]:
     """快速搜索
 
     Args:
@@ -72,25 +70,33 @@ async def quick_search(keyword: str) -> dict:
     Returns:
         包含专辑,歌手,歌曲的简略信息
     """
-    return await Api(**API["quick_search"]).update_params(key=keyword).result
+    resp = await get_session().get(
+        "https://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg",
+        params={
+            "key": keyword,
+        },
+    )
+    resp.raise_for_status()
+    return resp.json()["data"]
 
 
+@api_request("music.adaptor.SearchAdaptor", "do_search_v2")
 async def general_search(
     keyword: str,
     page: int = 1,
     highlight: bool = True,
-) -> dict:
+):
     """综合搜索
 
     Args:
-        keyword:   关键词
-        page:      页码
+        keyword: 关键词
+        page: 页码
         highlight: 是否高亮关键词
 
     Returns:
         包含直接结果,歌曲,歌手,专辑,歌单,mv等.
     """
-    params = {
+    return {
         "searchid": get_searchID(),
         "search_type": 100,
         "page_num": 15,
@@ -98,25 +104,25 @@ async def general_search(
         "page_id": page,
         "highlight": highlight,
         "grp": True,
-    }
-    return (await Api(**API["general_search"]).update_params(**params).result)["body"]
+    }, NO_PROCESSOR
 
 
+@api_request("music.search.SearchCgiService", "DoSearchForQQMusicMobile")
 async def search_by_type(
     keyword: str,
     search_type: SearchType = SearchType.SONG,
     num: int = 10,
     page: int = 1,
     highlight: bool = True,
-) -> list[dict]:
+):
     """搜索
 
     Args:
-        keyword:     关键词
+        keyword: 关键词
         search_type: 搜索类型
-        num:         返回数量
-        page:        页码
-        highlight:   是否高亮关键词
+        num: 返回数量
+        page: 页码
+        highlight: 是否高亮关键词
 
     Returns:
         搜索结果
@@ -130,16 +136,19 @@ async def search_by_type(
         "highlight": highlight,
         "grp": True,
     }
-    res = await Api(**API["mobile_search_by_type"]).update_params(**params).result
-    types = {
-        SearchType.SONG: "item_song",
-        SearchType.SINGER: "singer",
-        SearchType.ALBUM: "item_album",
-        SearchType.SONGLIST: "item_songlist",
-        SearchType.MV: "item_mv",
-        SearchType.LYRIC: "item_song",
-        SearchType.USER: "item_user",
-        SearchType.AUDIO_ALBUM: "item_audio",
-        SearchType.AUDIO: "item_song",
-    }
-    return res["body"][types[search_type]]
+
+    def _processor(data: dict[str, Any]) -> list[dict[str, Any]]:
+        types = {
+            SearchType.SONG: "item_song",
+            SearchType.SINGER: "singer",
+            SearchType.ALBUM: "item_album",
+            SearchType.SONGLIST: "item_songlist",
+            SearchType.MV: "item_mv",
+            SearchType.LYRIC: "item_song",
+            SearchType.USER: "item_user",
+            SearchType.AUDIO_ALBUM: "item_audio",
+            SearchType.AUDIO: "item_song",
+        }
+        return data["body"][types[search_type]]
+
+    return params, _processor
